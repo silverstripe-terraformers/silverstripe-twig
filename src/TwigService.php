5b03a47b7ce8ebf5899cb02652d4db60bedbdf76
cpenny <cpenny@silverstripe.com>
2020-06-29 08:05:27 +1200

    Move UrlToPath into service

--- src/TwigService.php
+++ src/TwigService.php
@@ -252,7 +252,7 @@ class TwigService
         $this->cache_path = sprintf(
             '%s%s.json',
             $this->getDestPath(),
-            URLtoPath($location, BASE_URL, false)
+            $this->UrlToPath($location, BASE_URL, false)
         );
 
         return $this->cache_path;
@@ -268,5 +268,53 @@ class TwigService
             defined('PUBLIC_PATH') ? PUBLIC_PATH : BASE_PATH,
             DIRECTORY_SEPARATOR
         );
+    }
+
+    public function UrlToPath($url, $baseURL = '', $domainBasedCaching = false): ?string
+    {
+        // parse_url() is not multibyte safe, see https://bugs.php.net/bug.php?id=52923.
+        // We assume that the URL hsa been correctly encoded either on storage (for SiteTree->URLSegment),
+        // or through URL collection (for controller method names etc.).
+        $urlParts = @parse_url($url);
+
+        // query strings are not yet supported so we need to bail is there is one present
+        if (!empty($urlParts['query'])) {
+            return null;
+        }
+
+        // Remove base folders from the URL if webroot is hosted in a subfolder)
+        $path = isset($urlParts['path'])
+            ? $urlParts['path']
+            : '';
+
+        if (mb_substr(mb_strtolower($path), 0, mb_strlen($baseURL)) == mb_strtolower($baseURL)) {
+            $urlSegment = mb_substr($path, mb_strlen($baseURL));
+        } else {
+            $urlSegment = $path;
+        }
+
+        // Normalize URLs
+        $urlSegment = trim($urlSegment, '/');
+
+        $filename = $urlSegment ?: "index";
+
+        if ($domainBasedCaching) {
+            if (!$urlParts) {
+                throw new \LogicException('Unable to parse URL');
+            }
+
+            if (isset($urlParts['host'])) {
+                $filename = $urlParts['host'] . '/' . $filename;
+            }
+        }
+
+        $dirName = dirname($filename);
+        $prefix = '';
+
+        if ($dirName != '/' && $dirName != '.') {
+            $prefix = $dirName . '/';
+        }
+
+        return $prefix . basename($filename);
     }
 }
